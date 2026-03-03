ba60c98a969c543b77c463957601684af4778885
abhishek-g2411 <abhishekgupta241120@gmail.com>
2026-03-04 01:09:05 +0530

    code of cpp count vowels from string

---
+++ practice1/volcnt.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  string s;
+  getline(cin, s);
+  int c = 0;
+  for (int i = 0; i < s.size(); i++) {
+    if (s[i] == 'a' || s[i] == 'e' || s[i] == 'o' || s[i] == 'i' ||
+        s[i] == 'u') {
+      cout << s[i] << " ";
+      // c++;g++ volcnt.cpp -o volcnt
+
+      c = c + 1;
+      // c+=1;
+    }
+  }
+  cout << endl;
+  cout << "No of vowels : " << c;
+}
