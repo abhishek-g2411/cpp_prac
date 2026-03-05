c55f0a5e2d3a5a8a563a2675fbc34ee0323e8611
abhishek-g2411 <abhishekgupta241120@gmail.com>
2026-03-06 02:52:15 +0530

    sum the digit

---
+++ pract2/c7.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+using namespace std;
+ int main(){
+     
+     int n;
+     
+     cin>> n;
+     int sum=0;
+     int product=1;
+     
+     while(n!=0){
+         sum +=( n % 10);
+         product = product * (n % 10);
+         n /=10;
+     }
+     
+     cout << sum<<" "<< product<<"\n";
+     
+     
+ }
+
+
+// input=22 
+// out put= 4 4
+
