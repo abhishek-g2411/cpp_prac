41ae6430d85ca8f0e9f5f479993fd7db24c0aa07
abhishek-g2411 <abhishekgupta241120@gmail.com>
2026-03-10 03:35:06 +0530

    ..

---
+++ pract2.0/c.2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        // your code goes here
+        if(x<2000){
+            cout<<"NO"<<endl;
+           
+        }
+                
+             else{
+                cout<<"yes"<<endl;
+            }
+            
+        
+    }
+	return 0;
+}
