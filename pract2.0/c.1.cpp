46875594970052e96a674f188f0286ed65061e0d
abhishek-g2411 <abhishekgupta241120@gmail.com>
2026-03-10 03:29:24 +0530

    qoes.cpp

---
+++ pract2.0/c.1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string s="abasdfgSRHGNKUH";
+    getlin(char c,S);
+    count =0;
+    for(i=o;i<s.lenght();i++){
+        if(s[i]==c)
+        count++;
+        }
+        return count;
+    }
