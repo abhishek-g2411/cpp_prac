d0395b2e86b6e7af432d4954eebcd1b94c68909e
abhishek-g2411 <abhishekgupta241120@gmail.com>
2026-03-11 02:15:06 +0530

    Binary Search code

---
+++ pract2.0/c.3.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int n, int key) {
+    int low = 0, high = n - 1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (arr[mid] == key)
+            return mid;     // element found
+
+        else if (arr[mid] < key)
+            low = mid + 1;  // search right side
+
+        else
+            high = mid - 1; // search left side
+    }
+
+    return -1; // element not found
+}
+
+int main() {
+    int arr[] = {2, 4, 6, 8, 10, 12};
+    int n = 6;
+    int key = 8;
+
+    int result = binarySearch(arr, n, key);
+
+    if (result != -1)
+        cout << "Element found at index: " << result;
+    else
+        cout << "Element not found";
+
+    return 0;
+}
